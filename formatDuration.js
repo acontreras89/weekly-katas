e4358dde3fccba5235353fb5640fb19f7d1c4de1
Aarón Contreras <aaron@redradix.com>
2021-11-09 12:29:01 +0100

    Return 'now' when 0 seconds

--- formatDuration.js
+++ formatDuration.js
@@ -15,14 +15,15 @@ const convertSecondsToUnitArray = seconds =>
 const convertToComponent = (amount, units) =>
   fn.pluralize(amount, COMPONENTS[units])
 
-const formatDuration = fn.pipe(
-  convertSecondsToUnitArray,
-  // NOTE do not rely on identity (for else clause) in case we get a negative value
-  fn.map(fn.branch(fn.gt(0), convertToComponent, fn.value(0))),
-  fn.filter(fn.identity),
-  fn.reverse,
-  fn.join(', '),
-  fn.replaceLast(', ', ' and '),
-)
+const formatDuration =
+  fn.branch(fn.lte(0), fn.value('now'), fn.pipe(
+    convertSecondsToUnitArray,
+    // NOTE do not rely on identity (for else clause) in case we get a negative value
+    fn.map(fn.branch(fn.gt(0), convertToComponent, fn.value(0))),
+    fn.filter(fn.identity),
+    fn.reverse,
+    fn.join(', '),
+    fn.replaceLast(', ', ' and '),
+  ))
 
 module.exports = formatDuration
