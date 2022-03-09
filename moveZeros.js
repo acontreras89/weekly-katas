d067a545ccd5bfe6ab270c898fad7aafddcd44b6
Aarón Contreras <aaron@redradix.com>
2022-03-09 10:25:04 +0100

    Add functional solution to moveZeros kata

--- moveZeros.js
+++ moveZeros.js
@@ -1,3 +1,8 @@
+const fn = require('./fn')
+
+const moveZeros = fn.pipe(fn.partition(fn.neq(0)), fn.flat)
+
+/*
 const partition = (arr, fn) => {
   const pass = []
   const fail = []
@@ -7,9 +12,11 @@ const partition = (arr, fn) => {
   return [pass, fail]
 }
 
+// IDEA invert condition (!== 0) and use flat instead of concat
 function moveZeros(arr) {
   const [zeros, rest] = partition(arr, item => item === 0)
   return rest.concat(zeros)
 }
+*/
 
 module.exports = moveZeros
