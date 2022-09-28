52f0ca779b8b15f45eb7c7242962be4e9946538a
Aarón Contreras <aaron@redradix.com>
2022-09-28 11:31:20 +0200

    Improve solution for countDeafRats kata

--- countDeafRats.js
+++ countDeafRats.js
@@ -1,34 +1,29 @@
 const RAT = 'O'
 const TAIL = '~'
 
-const countDeafRats = town => {
-  const [left, right] = town.split('P')
+const getLeftAndRightRats = town => town.split('P')
 
+const reverseString = str => str.split('').reverse().join('')
+
+const countRatsGoingLeft = town => {
   let rats = 0
+  for (let i = 0; i <= town.length; i++) {
+    const ch = town[i]
 
-  for (let i = 0; i <= left.length; i++) {
-    const ch = left[i]
-
-    if (ch === RAT) {
-      rats++
-      i++
-    } else if (ch === TAIL) {
-      i++
-    }
-  }
-
-  for (let i = 0; i <= right.length; i++) {
-    const ch = right[i]
-
-    if (ch === TAIL) {
-      rats++
-      i++
-    } else if (ch === RAT) {
-      i++
-    }
+    if (ch === RAT) rats++
+    // NOTE skip rest of the rat
+    if (ch === RAT || ch === TAIL) i++
   }
 
   return rats
+}
+
+const countDeafRats = town => {
+  const [left, right] = getLeftAndRightRats(town)
+
+  const allRatsGoingRight = left + reverseString(right)
+
+  return countRatsGoingLeft(allRatsGoingRight)
 }
 
 module.exports = countDeafRats
