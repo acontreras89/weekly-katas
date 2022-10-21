2c249c5d6846b50ef16e8c0adb3972ec419dc0bb
Aarón Contreras <aaron@redradix.com>
2022-10-21 17:37:30 +0200

    Rewrite countBits kata using fn utils

--- countBits.js
+++ countBits.js
@@ -1,3 +1,12 @@
-const countBits = n => n.toString(2).match(/1/g)?.length ?? 0
+const fn = require('./fn')
+
+const countBits = fn.pipe(
+  fn.toBinary,
+  fn.toCharArray,
+  fn.filter(fn.eq('1')),
+  fn.length
+)
+
+// const countBits = n => n.toString(2).match(/1/g)?.length ?? 0
 
 module.exports = countBits
