30d6bafd14b154b47b709d5ed1d9b832b07f7297
Aarón Contreras <aaron@redradix.com>
2022-10-21 17:35:00 +0200

    Rewrite getCount kata using fn utils

--- getCount.js
+++ getCount.js
@@ -1,3 +1,10 @@
-const getCount = str => str.match(/[aeiou]/g)?.length ?? 0
+const fn = require('./fn')
+
+const isVowel = ch =>
+  ch === 'a' || ch === 'e' || ch === 'i' || ch === 'o' || ch === 'u'
+
+const getCount = fn.pipe(fn.split(''), fn.filter(isVowel), fn.length)
+
+// const getCount = str => str.match(/[aeiou]/g)?.length ?? 0
 
 module.exports = getCount
